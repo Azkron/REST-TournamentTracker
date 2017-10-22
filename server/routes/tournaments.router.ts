cb4965646d6d3189ef123e83542f9eea71996125
Touré Tidiane <touretidiane@hotmail.com>
2017-10-22 13:05:05 +0200

    V3 with promises

--- server/routes/tournaments.router.ts
+++ server/routes/tournaments.router.ts
@@ -9,25 +9,72 @@ export class TournamentsRouter {
         this.router = Router();
         this.router.get('/', this.getAll);
         this.router.post('/', this.create);
-        this.router.delete('/', this.deleteAll);
-        this.router.get('/:id', this.getOne);
-        this.router.put('/:id', this.update);
-        this.router.delete('/:id', this.deleteOne);
+        this.router.get('/:name', this.findByName);
+        this.router.get('/byId/:id', this.findById);
+        this.router.get('/byStartDate/:start', this.findByStartDate);
+        this.router.get('/byFinishDate/:finish', this.findByFinishDate);
+        this.router.get('/byMaxPlayers/:max', this.findByMaxPlayers);
+        this.router.get('/byStartRange/:start/:finish', this.getRange);
+        this.router.put('/:name', this.update);
+        this.router.delete('/:name', this.deleteOne);
+        this.router.delete('/:start/:finish', this.deleteRange);
     }
 
     public getAll(req: Request, res: Response, next: NextFunction) {
-        Tournament.find().sort({ pseudo: 'asc'}).exec((err, members) => {
-            if (err) res.send(err);
-            res.json(members);
-        });
+		Tournament.find().sort({ name: 'asc' })
+            .then(members => res.json(members))
+            .catch(err => res.json([]));
     }
 
-    public getOne(req: Request, res: Response, next: NextFunction) {
-        Tournament.find({ pseudo: req.params.id }, (err, members) => {
-            if (err) res.send(err);
-            res.json(members);
-        });
+    public findById(req: Request, res: Response, next: NextFunction) {
+        Tournament.find({ _id: req.params.id })
+            .then(member => res.json(member))
+            .catch(err => res.json([]));
+	
+	public findByName(req: Request, res: Response, next: NextFunction) {
+        Tournament.find({ name: req.params.name })
+            .then(member => res.json(member))
+            .catch(err => res.json([]));
     }
+
+    public findByStartDate(req: Request, res: Response, next: NextFunction) {
+        let d = new Date(req.params.start);
+        if (!isNaN(d.valueOf())) {
+            Tournament.find({ start: d }).sort({ name: 'asc' })
+                .then(members => res.json(members))
+                .catch(err => res.json([]));
+        }
+        else
+            res.json([]);
+    }
+
+    public findByFinishDate(req: Request, res: Response, next: NextFunction) {
+        let d = new Date(req.params.finish);
+        if (!isNaN(d.valueOf())) {
+            Tournament.find({ finish: d }).sort({ name: 'asc' })
+                .then(members => res.json(members))
+                .catch(err => res.json([]));
+        }
+        else
+            res.json([]);
+    }
+
+    public findByMaxPlayers(req: Request, res: Response, next: NextFunction) {
+        Tournament.find({ maxPlayers: req.params.max }).sort({ name: 'asc' })
+            .then(members => res.json(members))
+            .catch(err => res.json([]));
+    }
+
+    public getRange(req: Request, res: Response, next: NextFunction) {
+        let d1 = new Date(req.params.start);
+        let d2 = new Date(req.params.finish);
+        if (isNaN(d1.valueOf()) || isNaN(d2.valueOf()))
+            res.json({errmsg: 'bad date range'});
+        else {
+            Tournament.find({ start: { $gte: d1, $lte: d2 } })
+                .then(members => res.json(members))
+                .catch(err => res.json([]));
+        }
 
     public createPromise(req: Request, res: Response, next: NextFunction) {
         let tournament = new Tournament(req.body);
@@ -39,42 +86,36 @@ export class TournamentsRouter {
 
     public create(req: Request, res: Response, next: NextFunction) {
         let tournament = new Tournament(req.body);
-        tournament.save(function (err, task) {
-            if (err) res.send(err);
-            res.json(task);
-        });
+        tournament.save()
+            .then(r => res.json(r))
+            .catch(err => res.json(err));
     }
 
 
     public update(req: Request, res: Response, next: NextFunction) {
-        console.log(req.body);
-        let tournament = new Tournament(req.body);
-        Tournament.findOneAndUpdate({ pseudo: req.params.id },
+		Tournament.findOneAndUpdate({ name: req.params.name },
             req.body,
-            { new: true },  // pour renvoyer le document modifié
-            function (err, task) {
-                console.log(err, task);
-                if (err)
-                    res.send(err);
-                res.json(task);
-            });
+			{ new: true })  // pour renvoyer le document modifié
+            .then(r => res.json(r))
+            .catch(err => res.json(err));
     }
 
     public deleteOne(req: Request, res: Response, next: NextFunction) {
-        Tournament.findOneAndRemove({ name: req.params.id },
-            function (err, task) {
-                if (err)
-                    res.send(err);
-                res.json(task);
-            });
+		Tournament.findOneAndRemove({ name: req.params.name })
+            .then(r => res.json(true))
+            .catch(err => res.json(err));
     }
-
-    public deleteAll(req: Request, res: Response, next: NextFunction) {
-        Tournament.remove({},
-            function (err) {
-                if (err)
-                    res.send(err);
-                res.json({status: 'ok'});
-            });
-    }
+	
+    public deleteRange(req: Request, res: Response, next: NextFunction) {
+        let d1 = new Date(req.params.start);
+        let d2 = new Date(req.params.finish);
+        if (isNaN(d1.valueOf()) || isNaN(d2.valueOf()))
+            res.json({errmsg: 'bad date range'});
+        else {
+            Tournament.find({ start: { $gte: d1, $lte: d2 } })
+                .remove()
+                .then(r => res.json(true))
+                .catch(err => res.json(err));
+        }
+	}		
 }
