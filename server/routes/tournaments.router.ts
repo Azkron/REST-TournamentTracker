2160608635609516d3ee2784819dbc7992a93152
AZKRON\Hugo <hugobeny@gmail.com>
2018-01-11 20:27:33 +0100

    fixed subscribe one

--- server/routes/tournaments.router.ts
+++ server/routes/tournaments.router.ts
@@ -29,74 +29,7 @@ export class TournamentsRouter {
         this.router.delete('/:start/:finish', this.deleteRange);
     }
     
-    public subscribeCurrent(req: Request, res: Response, next: NextFunction) {
-        
-        let memberPseudo = AuthentificationRouter.getPseudo(req);
-        Member.find({ pseudo: memberPseudo }).exec((err, m) =>{
-            if(err) res.send(err);
-            Tournament.find({ _id: req.body._id }).populate('members').populate('games').exec((err, t) =>{
-                if(err) res.send(err);
-                let currMember = m[0];
-                let currTournament = t[0];
-                
-                currTournament.members.push(currMember);
-
-                let count = 0;
-                let maxCount = currTournament.members.length;
-
-                if(currTournament.members.length <=1)
-                {
-                    currTournament.save()
-                        .then(t => res.json(t))
-                        .catch(err => res.json(err));
-                }
-                else for(let member of currTournament.members)
-                {
-                    count++;
-                    if(member.pseudo != currMember.pseudo)
-                    {
-                        console.log("subscribeCurrent member.pseudo = " + member.pseudo);
-                        let game = new Game();
     
-                        game.player_1 = currMember.pseudo;
-                        game.player_2 = member.pseudo;
-                        game.tournament = currTournament;
-                        console.log("subscribeCurrent game = ");
-                        console.log( game);
-                        
-                        game.save().then(g => {
-                            if(err) res.send(err);
-                            //currTournament.games.push(g);
-                            currTournament.games.push(g);
-                            if(count == maxCount)
-                            {
-                                console.log("subscribeCurrent g.player_1 = ");
-                                console.log(g.player_1);
-                                currTournament.save()
-                                    .then(t => {
-                                        res.json(t.populate('members').populate('games'));
-                                    })
-                                    .catch(err => res.json(err));
-                            }
-                        });
-                    }
-                }
-
-
-                
-            });
-    
-            // Tournament.findOneAndUpdate({ name: tournament.name },
-            //     tournament,
-            //     { new: true })  // pour renvoyer le document modifié
-            //     .populate('members').populate('games').then(r => {
-            //         // console.log("r =>" +r)
-            //         res.json(r)
-            //     })
-            //     .catch(err => res.json(err));
-
-        });
-    }
 
     public getCountTournament(req: Request, res: Response, next: NextFunction) {
         Tournament.count({}).exec((err, count) => {
@@ -261,8 +194,87 @@ export class TournamentsRouter {
                         } 
                 }
             } );
+    }
+    
+    public subscribeCurrent(req: Request, res: Response, next: NextFunction) {
+        let memberPseudo = AuthentificationRouter.getPseudo(req);
 
-        
+        Member.find({ pseudo: memberPseudo }).exec((err, m) =>{
+            if(err) res.send(err);
+
+            console.log("updateSubscriptions req.body");
+            console.log(req.body);
+            let loop : boolean; 
+            let first : boolean;
+            let addListGame : boolean;
+            let currMember = m[0];
+
+            Tournament.findOneAndUpdate({ _id: req.body._id }, req.body,{ new: true })
+                .populate('members')
+                .then(t => {
+                    t.members.push(currMember);
+                    console.log("updateSubscritions t.members AFTER");
+                    console.log(t.members);
+                    console.log("updateSubscriptions t.games BEFORE");
+                    console.log(t.games);
+                    console.log("updateSubscriptions t.games AFTER");
+                    while(t.games.length> 0)
+                        t.games.pop();
+                    
+                    console.log(t.games);
+                    if(t.members.length <= 1)
+                    {
+                        t.save().then(t =>res.json(t));
+                    }
+                    else
+                    {
+                        for(let i = 0; i < t.members.length-1; ++i)
+                        {
+                            for(let z = i+1; z < t.members.length; ++z)
+                            {
+                                loop = true; addListGame = true;
+                                let m = t.members[i];
+                                let m1 = t.members[z];
+                                console.log("members "+m.pseudo+", "+m1.pseudo);
+                                if(m.pseudo !== m1.pseudo) 
+                                {
+                                    for(let game of t.games) 
+                                    {
+                                        if(loop) {
+                                            if (m.pseudo == game.player_2 && m1.pseudo == game.player_1) {
+                                                loop = false;
+                                                addListGame = false;
+                                            }
+                                        }
+                                        else    
+                                            break;
+                                    }
+
+                                    if(addListGame)
+                                    {
+                                        let g = new Game();
+                                        g.player_1 = m.pseudo;
+                                        g.player_2 = m1.pseudo;
+                                        g.tournament = t;
+                                        g.save().then(g => {
+                                            // console.log("game to add = ");
+                                            // console.log(g);
+                                            t.games.push(g);
+                                            if(i == t.members.length-2 && z == t.members.length-1)
+                                            {
+                                                // console.log("tournament after games = ");
+                                                // console.log(t);
+                                                t.save().then(t =>res.json(t));
+                                                
+                                            }
+                                        });
+                                    }
+                                }
+                            } 
+                        }
+                    }
+                });
+        });
     }
 
     public deleteOne(req: Request, res: Response, next: NextFunction) {
@@ -292,5 +304,78 @@ export class TournamentsRouter {
                 .then(r => res.json(true))
                 .catch(err => res.json(err));
         }
-	}		
+    }		
+    
+    // public subscribeCurrent(req: Request, res: Response, next: NextFunction) {
+        
+    //     let memberPseudo = AuthentificationRouter.getPseudo(req);
+    //     Member.find({ pseudo: memberPseudo }).exec((err, m) =>{
+    //         if(err) res.send(err);
+    //         Tournament.find({ _id: req.body._id }).populate('members').populate('games').exec((err, t) =>{
+    //             if(err) res.send(err);
+    //             let currMember = m[0];
+    //             let currTournament = t[0];
+
+    //             for(let member of currTournament.members)
+    //                 if(member.pseudo == currMember.pseudo)
+    //                     res.send("Member already subscribed");
+                
+    //             currTournament.members.push(currMember);
+
+    //             let count = 0;
+    //             let maxCount = currTournament.members.length;
+
+    //             if(currTournament.members.length <=1)
+    //             {
+    //                 currTournament.save()
+    //                     .then(t => res.json(t))
+    //                     .catch(err => res.json(err));
+    //             }
+    //             else for(let member of currTournament.members)
+    //             {
+    //                 count++;
+    //                 if(member.pseudo != currMember.pseudo)
+    //                 {
+    //                     console.log("subscribeCurrent member.pseudo = " + member.pseudo);
+    //                     let game = new Game();
+    
+    //                     game.player_1 = currMember.pseudo;
+    //                     game.player_2 = member.pseudo;
+    //                     game.tournament = currTournament;
+    //                     console.log("subscribeCurrent game = ");
+    //                     console.log( game);
+                        
+    //                     game.save().then(g => {
+    //                         if(err) res.send(err);
+    //                         //currTournament.games.push(g);
+    //                         currTournament.games.push(g);
+    //                         if(count == maxCount)
+    //                         {
+    //                             console.log("subscribeCurrent g.player_1 = ");
+    //                             console.log(g.player_1);
+    //                             currTournament.save()
+    //                                 .then(t => {
+    //                                     res.json(t.populate('members').populate('games'));
+    //                                 })
+    //                                 .catch(err => res.json(err));
+    //                         }
+    //                     });
+    //                 }
+    //             }
+
+
+                
+    //         });
+    
+    //         // Tournament.findOneAndUpdate({ name: tournament.name },
+    //         //     tournament,
+    //         //     { new: true })  // pour renvoyer le document modifié
+    //         //     .populate('members').populate('games').then(r => {
+    //         //         // console.log("r =>" +r)
+    //         //         res.json(r)
+    //         //     })
+    //         //     .catch(err => res.json(err));
+
+    //     });
+    // }
 }
